ed7f7c3ac24b066b1518daddc67610299a190ef5
joschua <joschuagosda@gmail.com>
2021-01-25 15:57:12 +0100

    changed pom file to not depreciate OAuth2RestOperations and added bean to access the GitLab Api

--- project/src/main/java/edu/tum/ase/project/service/ProjectService.java
+++ project/src/main/java/edu/tum/ase/project/service/ProjectService.java
@@ -4,6 +4,11 @@ import edu.tum.ase.project.model.Project;
 import edu.tum.ase.project.model.SourceFile;
 import edu.tum.ase.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -40,5 +45,12 @@ public class ProjectService {
         project.setName(name);
         projectRepository.save(project);
         return project;
+    }
+
+    // add Bean to support searching for users by the GitLab Api
+    @Bean
+    public OAuth2RestOperations restTemplate (OAuth2ClientContext context) {
+        ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+        return new OAuth2RestTemplate(details, context);
     }
 }
